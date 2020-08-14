0ab3ccfe0231ea291db5048a00c7fc66e1addaaa
JasWoolieX <jbamrah@woolworths.com.au>
2020-08-14 16:33:42 +1000

    Update gitsync

--- git-sync.sh
+++ git-sync.sh
@@ -7,6 +7,11 @@ SOURCE_BRANCH=$2
 DESTINATION_REPO=$3
 DESTINATION_BRANCH=$4
 
+SOURCE_REPO="https://JasWoolieX:jack%40663481@github.com/woolworthslimited/woolworths-mobile-api-automation.git"
+SOURCE_BRANCH="master"
+DESTINATION_REPO="https://JasWoolieX:jack%40663481@github.com/JasWoolieX/woolworths-mobile-api-automation.git"
+DESTINATION_BRANCH="master"
+
 if ! echo $SOURCE_REPO | grep '.git'
 then
   if [[ -n "$SSH_PRIVATE_KEY" ]]
@@ -38,6 +43,9 @@ git remote add destination "$DESTINATION_REPO"
 git fetch source '+refs/heads/*:refs/heads/*' --update-head-ok
 
 # Print out all branches
-git --no-pager branch -a -vv
-
-git push destination "${SOURCE_BRANCH}:${DESTINATION_BRANCH}" -f
+#git --no-pager branch -a -vv
+echo "Checking git status"
+git status
+git push destination "${SOURCE_BRANCH}:${DESTINATION_BRANCH}"
+#git pull https://hostname/ORIGINAL_OWNER/ORIGINAL_REPOSITORY.git BRANCH_NAME
+#git push destination "${SOURCE_BRANCH}:${DESTINATION_BRANCH}" -f
