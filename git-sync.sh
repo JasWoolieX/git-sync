d016e4cfef203480e7ce0e23ed047e3ea504140e
JasWoolieX <jbamrah@woolworths.com.au>
2020-08-18 16:42:56 +1000

    Update git-sync.sh

--- git-sync.sh
+++ git-sync.sh
@@ -30,33 +30,15 @@ fi
 
 echo "SOURCE=$SOURCE_REPO:$SOURCE_BRANCH"
 echo "DESTINATION=$DESTINATION_REPO:$DESTINATION_BRANCH"
-echo "Checking git status"
-echo "Check that this is being printed"
-#git clone "$SOURCE_REPO" /root/source --origin source && cd /root/source
-#git remote add destination "$DESTINATION_REPO"
+
+git clone "$SOURCE_REPO" /root/source --origin source && cd /root/source
+git remote add destination "$DESTINATION_REPO"
 
 # Pull all branches references down locally so subsequent commands can see them
-#git fetch source '+refs/heads/*:refs/heads/*' --update-head-ok
+git fetch source '+refs/heads/*:refs/heads/*' --update-head-ok
 
 # Print out all branches
-#git --no-pager branch -a -vv
-echo "Checking git status"
-git status
-git fetch upstream
-git checkout master
-git merge upstream/master
-git push upstream master
-#git rebase upstream/master
-#git push -f origin master
-#git push origin master
-#git push "${DESTINATION_REPO}" master
-#git checkout master
-#git merge upstream/master
-#git push
+git --no-pager branch -a -vv
 
-
-
-#git pull $SOURCE_REPO $SOURCE_BRANCH
-#git push $DESTINATION_REPO master
-#git push destination "${SOURCE_BRANCH}:${DESTINATION_BRANCH}"
+git push destination "${SOURCE_BRANCH}:${DESTINATION_BRANCH}"
 
