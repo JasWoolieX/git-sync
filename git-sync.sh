a711ce705f989dd6cd6da58730aa3c7e8f268f8b
JasWoolieX <jbamrah@woolworths.com.au>
2020-08-18 18:20:50 +1000

    Update git-sync.sh

--- git-sync.sh
+++ git-sync.sh
@@ -36,6 +36,7 @@ git fetch source '+refs/heads/*:refs/heads/*' --update-head-ok
 
 echo "git status"
 git status
+git remote add destination "$SOURCE_REPO"
 echo "git status after"
 #echo "SOURCE=$SOURCE_REPO:$SOURCE_BRANCH"
 #echo "DESTINATION=$DESTINATION_REPO:$DESTINATION_BRANCH"
